3d280f42c20a72149949eae78eca07f3970b30e1
matinfar.a <alimatinfar1376@gmail.com>
2025-12-13 19:19:49 +0330

    [BUGFIX]: fixed typescript bugs

--- components/Form/DatePicker/utils/getMiladiFormattedDate.tsx
+++ components/Form/DatePicker/utils/getMiladiFormattedDate.tsx
@@ -1,6 +1,7 @@
 import {DateObject} from "react-multi-date-picker";
 
 function getMiladiFormattedDate(value:string | Date | DateObject) {
+  // @ts-ignore
   return new DateObject(new Date(value)).format('YYYY-MM-DD')
 }
 
