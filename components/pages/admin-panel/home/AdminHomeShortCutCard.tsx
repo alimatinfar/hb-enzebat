68508abe374309db8fff8b1ff4d7e446ec59f6fb
a.matinfar <alimatinfar1376@gmail.com>
2026-04-03 18:17:47 +0330

    [BUGFIX]: fixed some typescript bugs

--- components/pages/admin-panel/home/AdminHomeShortCutCard.tsx
+++ components/pages/admin-panel/home/AdminHomeShortCutCard.tsx
@@ -19,7 +19,7 @@ export type AdminHomeShortCutCardProps = {
 }
 
 function AdminHomeShortCutCard(
-  {title, lastTitle, isLoading, error, RowIcon, link, items}: AdminHomeShortCutCardProps
+  {title, lastTitle, RowIcon, link, items}: AdminHomeShortCutCardProps
 ) {
   return (
     <Card>
@@ -28,35 +28,35 @@ function AdminHomeShortCutCard(
       </p>
 
       <div className='flex flex-col border-b border-gray-300 mb-2 pb-2'>
-        <RenderLogic
-          isLoading={isLoading} error={error} renderLogicDefaultContainerMinHeight='min-h-auto'
-          loadingElement={(
-            <div className='flex flex-col w-full'>
-              {[1, 2, 3].map((_, index) => (
-                <div key={index} className='h-10 w-full py-2'>
-                  <Skeleton size='h-full w-full' bgClass='bg-gray-2'/>
-                </div>
-              ))}
-            </div>
-          )}
-        >
-          {items.map((item) => (
-            <div key={item.id} className='py-2 flex items-center justify-between'>
-              <div className='flex items-center gap-x-1'>
-                <RowIcon textColor='text-gray-600' width={14} height={14}/>
-                <span>
+        {/*<RenderLogic*/}
+        {/*  isLoading={isLoading} error={error} renderLogicDefaultContainerMinHeight='min-h-auto'*/}
+        {/*  loadingElement={(*/}
+        {/*    <div className='flex flex-col w-full'>*/}
+        {/*      {[1, 2, 3].map((_, index) => (*/}
+        {/*        <div key={index} className='h-10 w-full py-2'>*/}
+        {/*          <Skeleton size='h-full w-full' bgClass='bg-gray-2'/>*/}
+        {/*        </div>*/}
+        {/*      ))}*/}
+        {/*    </div>*/}
+        {/*  )}*/}
+        {/*>*/}
+        {items.map((item) => (
+          <div key={item.id} className='py-2 flex items-center justify-between'>
+            <div className='flex items-center gap-x-1'>
+              <RowIcon textColor='text-gray-600' width={14} height={14}/>
+              <span>
                   {item.name}
                 </span>
-              </div>
+            </div>
 
-              {hasRole('ADMIN') && (
-                <span className='text-gray-500 text-sm'>
+            {hasRole('ADMIN') && (
+              <span className='text-gray-500 text-sm'>
                   {item.cityName}
                 </span>
-              )}
-            </div>
-          ))}
-        </RenderLogic>
+            )}
+          </div>
+        ))}
+        {/*</RenderLogic>*/}
       </div>
 
       <CardRowLink link={link}>
