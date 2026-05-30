2b0e8785b39077ee34b5cb5903453d31c29940d0
a.matinfar <alimatinfar1376@gmail.com>
2026-05-30 05:45:47 +0330

    [REFACTOR]: moved logics of AdminClassesPage to useAdminClassesPage.ts

--- app/admin-panel/classes/page.tsx
+++ app/admin-panel/classes/page.tsx
@@ -1,39 +1,17 @@
 'use client'
 
-import useFetchData from "@/request/hooks/useFetchData";
-import APIES from "@/request/constances/apies";
-import {useMemo} from "react";
 import RenderLogic from "@/components/others/RenderLogic/RenderLogic";
-import {AdminClassResponseType} from "@/components/pages/admin-panel/classes/AdminPanelClasses.types";
 import AdminLayout from "@/components/layouts/AdminLayout";
 import AdminClassCard from "@/components/pages/admin-panel/classes/AdminClassCard";
 import PageTitleWithAddButton from "@/components/others/PageTitle/PageTitleWithAddButton";
-import {useRouter} from "next/navigation";
-import ROUTER_LINKS from "@/constances/routerLinks";
+import useAdminClassesPage from "@/components/pages/admin-panel/classes/hooks/useAdminClassesPage";
 
 
 function AdminClassesPage() {
 
-  //TODO added pagination
   const {
-    data, isFetching, error
-  } = useFetchData<{ classes: AdminClassResponseType[] }>({
-    axiosConfig: {
-      url: APIES.ADMIN_CLASSES
-    },
-    disableThrowErrorToast: true
-  })
-
-  const classesList = useMemo(function () {
-    if (!data) return []
-    return data.classes
-  }, [data])
-
-  const router = useRouter()
-
-  const goToAddClassPage = function () {
-    router.push(ROUTER_LINKS.ADMIN_PANEL_CLASS_ADD)
-  }
+    goToAddClassPage, isFetching, error, classesList
+  } = useAdminClassesPage()
 
   return (
     <AdminLayout>
---
+++ components/pages/admin-panel/classes/hooks/useAdminClassesPage.ts
@@ -0,0 +1,36 @@
+import useFetchData from "@/request/hooks/useFetchData";
+import {AdminClassResponseType} from "@/components/pages/admin-panel/classes/AdminPanelClasses.types";
+import APIES from "@/request/constances/apies";
+import {useMemo} from "react";
+import {useRouter} from "next/navigation";
+import ROUTER_LINKS from "@/constances/routerLinks";
+
+function useAdminClassesPage() {
+
+  //TODO added pagination
+  const {
+    data, isFetching, error
+  } = useFetchData<{ classes: AdminClassResponseType[] }>({
+    axiosConfig: {
+      url: APIES.ADMIN_CLASSES
+    },
+    disableThrowErrorToast: true
+  })
+
+  const classesList = useMemo(function () {
+    if (!data) return []
+    return data.classes
+  }, [data])
+
+  const router = useRouter()
+
+  const goToAddClassPage = function () {
+    router.push(ROUTER_LINKS.ADMIN_PANEL_CLASS_ADD)
+  }
+
+  return {
+    goToAddClassPage, isFetching, error, classesList
+  }
+}
+
+export default useAdminClassesPage;
